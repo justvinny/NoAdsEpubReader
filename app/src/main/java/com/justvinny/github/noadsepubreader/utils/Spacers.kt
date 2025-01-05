a2d15d21b681d7f793cd081d648be6faffd43910
Vinson Beduya <vinsonemb.151994@gmail.com>
2025-01-05 14:01:06 +1300

    Added fill spacers

---
+++ app/src/main/java/com/justvinny/github/noadsepubreader/utils/Spacers.kt
@@ -0,0 +1,17 @@
+package com.justvinny.github.noadsepubreader.utils
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ColumnScope.VerticalFillSpacer(weight: Float = 1f) {
+    Spacer(modifier = Modifier.weight(weight))
+}
+
+@Composable
+fun RowScope.HorizontalFillSpacer(weight: Float = 1f) {
+    Spacer(modifier = Modifier.weight(weight))
+}
